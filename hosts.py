8888647509d4867703c3c20ae45767819973b291
Daan de Goede <ddegoede@schubergphilis.com>
2022-11-28 09:28:30 +0100

    Adding flip resource state feature

--- hosts.py
+++ hosts.py
@@ -26,12 +26,13 @@ from cosmicops import CosmicOps, logging
               help='Name of the CloudMonkey profile containing the credentials')
 @click.option('--disable', is_flag=True, help='Disable host(s)')
 @click.option('--enable', is_flag=True, help='Enable host(s)')
+@click.option('--flip-resource-state', is_flag=True, help='Flip the resource state of all hosts in cluster')
 @click.option('--hypervisor', '-h', type=int, help='Hypervisor number')
 @click.option('--end', '-e', type=int, help='Hypervisor number to end, requires -h option as start number')
 @click.option('--dry-run/--exec', is_flag=True, default=True, show_default=True, help='Enable/disable dry-run')
 @click_log.simple_verbosity_option(logging.getLogger(), default="INFO", show_default=True)
 @click.argument('cluster')
-def main(profile, disable, enable, hypervisor, end, dry_run, cluster):
+def main(profile, disable, enable, flip_resource_state, hypervisor, end, dry_run, cluster):
     """Enable or disable a range of hosts in a specific pod."""
 
     click_log.basic_config()
@@ -44,7 +45,7 @@ def main(profile, disable, enable, hypervisor, end, dry_run, cluster):
         logging.error(f"--end cannot be equal or smaller then --hypervisor ({end}<={hypervisor})")
         sys.exit(1)
 
-    if (disable or enable) and dry_run:
+    if (flip_resource_state or disable or enable) and dry_run:
         logging.info('Running in dry-run mode, will only show changes')
 
     co = CosmicOps(profile=profile, dry_run=dry_run, log_to_slack=log_to_slack)
@@ -55,9 +56,9 @@ def main(profile, disable, enable, hypervisor, end, dry_run, cluster):
         logging.error(f"Could not find cluster :'{cluster}'")
         sys.exit(1)
 
-    # show hosts resource state if not changing something
+    pod = cluster.split('-')[0]
+
     if hypervisor and (disable or enable):
-        pod = cluster.split('-')[0]
         if not end:
             end = hypervisor
             logging.info(f"Going to change {pod}-hv{hypervisor:02d}")
@@ -83,6 +84,19 @@ def main(profile, disable, enable, hypervisor, end, dry_run, cluster):
                 raise RuntimeError(f"Failed to disable host '{hostname}'")
             if enable and not host.enable():
                 raise RuntimeError(f"Failed to enable host '{hostname}'")
+        logging.info(f"Done!\n")
+
+    if flip_resource_state:
+        logging.info(f"Going to flip the resource state in cluster: {cluster}")
+
+        for host in sorted(clstr.get_all_hosts(), key=lambda h: h['name']):
+            if host['resourcestate'] == 'Disabled':
+                if not host.enable():
+                    raise RuntimeError(f"Failed to enable host '{host['name']}'")
+            elif host['resourcestate'] == 'Enabled':
+                if not host.disable():
+                    raise RuntimeError(f"Failed to disable host '{host['name']}'")
+
         logging.info(f"Done!\n")
 
     hosts = sorted(clstr.get_all_hosts(), key=lambda h: h['name'])
