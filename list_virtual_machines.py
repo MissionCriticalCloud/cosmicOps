7491b8e52686664f3d80bd1e856f55392092190c
Remi Bergsma <github@remi.nl>
2023-04-25 15:55:34 +0200

    Fixup

--- list_virtual_machines.py
+++ list_virtual_machines.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3# Copyright 2020, Schuberg Philis B.V
+#!/usr/bin/env python3
+# Copyright 2020, Schuberg Philis B.V
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
